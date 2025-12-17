f1f79c0884519e076be249242a512616d2724f88
Joelchocho2005 <joel.jachc@gmail.com>
2025-12-17 08:42:44 -0500

    Version:17.12.2025-8:42

--- src/main/com/PyJVuelos/persistencia/repositorio/interfaces/IRepositorioUsuario.java
+++ src/main/com/PyJVuelos/persistencia/repositorio/interfaces/IRepositorioUsuario.java
@@ -1,5 +1,9 @@
 package com.PyJVuelos.persistencia.repositorio.interfaces;
 
+import com.PyJVuelos.persistencia.modelos.entidades.UsuarioEntidad;
+
 public interface IRepositorioUsuario {
+    UsuarioEntidad guardarUsuario(UsuarioEntidad usuario);
+    UsuarioEntidad buscarUsuarioPorEmail(String email);
 
 }
